4a797f00d80d52cd57a4590efb4b81898f9af078
Peter Gustafsson <peter.gustafsson@knowit.se>
2020-01-16 18:21:10 +0100

    Added a minified version of the bundle version

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,26 +2,50 @@
 
 const commonjs = require('rollup-plugin-commonjs');
 const nodeResolve = require('@rollup/plugin-node-resolve')
+// const terser = require('rollup-plugin-terser')
+import { terser } from "rollup-plugin-terser";
+
 
 const filename = 'accessible-bootstrap-carousel'
 
-const plugins = [
-    commonjs(),
-    // Find node modules to include in bundle
-    nodeResolve()
-  ]
+const banner = 
+`/*
+* @preserve This version of ${filename} includes a bundled version of "magnific-popup"
+* NOTE! You are required to include boostrap and jQuery yourself.
+*/
+`;
 
 const rollupConfig = {
     input: './src/' + filename + '.js',
-    output: {
-    //   banner,
-      file: 'dist/' + filename + '.bundle.js',
-      format: 'iife',
-      globals:{'jquery':'$'},
+    output: 
+    [{
+        banner,
+        file: 'dist/' + filename + '.bundle.js',
+        format: 'iife',
+        globals: { 'jquery': '$' },
     },
+    {
+        banner,
+        file: 'dist/' + filename + '.bundle.min.js',
+        format: 'iife',
+        globals: { 'jquery': '$' },
+        plugins: [terser({
+            output: {
+                comments: function(node, comment) {
+                  if (comment.type == "comment2") {
+                    // Preserve multiline comments mathing these notations
+                    return /@preserve|@license|@cc_on/i.test(comment.value);
+                  }
+                }
+              }
+            
+          })]
+    }],
     external: ['jquery'],
-    plugins
-  }
+    plugins :  [
+        commonjs(),
+        // Find node modules to include in bundle
+        nodeResolve()    ]
+}
 
-  module.exports = rollupConfig
-  
+module.exports = rollupConfig
