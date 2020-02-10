b4a9f9d9c334e4b4876f39b3226375b7f9d643d3
Daniel Gustafsson <daniel.gustafsson@knowit.se>
2020-02-10 14:02:50 +0100

    Added babel

--- dist/accessible-bootstrap-carousel.js
+++ dist/accessible-bootstrap-carousel.js
@@ -1,22 +1,29 @@
-import $ from 'jquery';
-import { initDescriptionPlacement, calculateDescriptionHeight } from './js/abc-descriptions';
-import { initCarouselPopupLayout, openCarouselPopup } from './js/abc-popup';
-const $accesibleCarousels = $('.accessible-carousel');
+"use strict";
 
-let initAccessibleCarousels = function () {
+var _jquery = _interopRequireDefault(require("jquery"));
+
+var _abcDescriptions = require("./js/abc-descriptions");
+
+var _abcPopup = require("./js/abc-popup");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var $accesibleCarousels = (0, _jquery.default)('.accessible-carousel');
+
+var initAccessibleCarousels = function initAccessibleCarousels() {
   $accesibleCarousels.each(function (index, value) {
-    const $this = $(this);
-    const $descriptions = $this.find('.carousel-descriptions');
-    const elementId = '#' + $this.attr('id');
-    let interval = $this.data('interval');
-    let indicatorCurrentText = '(Current)';
-    let descriptionPlacement = $this.data('description-placement');
-    let $from;
-    let $to;
-    let $fromDescription;
-    let $toDescription;
-    let $fromIndicator;
-    let $toIndicator;
+    var $this = (0, _jquery.default)(this);
+    var $descriptions = $this.find('.carousel-descriptions');
+    var elementId = '#' + $this.attr('id');
+    var interval = $this.data('interval');
+    var indicatorCurrentText = '(Current)';
+    var descriptionPlacement = $this.data('description-placement');
+    var $from;
+    var $to;
+    var $fromDescription;
+    var $toDescription;
+    var $fromIndicator;
+    var $toIndicator;
     $this.find('.carousel-item:first-child').addClass('active');
     $this.find('.carousel-indicators li:first-child').addClass('active');
     $this.carousel({
@@ -31,72 +38,72 @@ let initAccessibleCarousels = function () {
       if (descriptionPlacement == "under") {
         $fromDescription = '.carousel-descriptions__item:nth-child(' + (ev.from + 1) + ')';
         $toDescription = '.carousel-descriptions__item:nth-child(' + (ev.to + 1) + ')';
-        $(this).find($fromDescription).fadeOut(300);
-        $(this).find($toDescription).delay(300).fadeIn(300);
+        (0, _jquery.default)(this).find($fromDescription).fadeOut(300);
+        (0, _jquery.default)(this).find($toDescription).delay(300).fadeIn(300);
       }
     });
     $this.on('slid.bs.carousel', function onSlide(ev) {
-      $(this).find($from).attr('aria-hidden', 'true');
-      $(this).find($to).removeAttr('aria-hidden');
-      $(this).find($fromIndicator).find('.carousel-tools__indicators-current').html('');
-      $(this).find($toIndicator).find('.carousel-tools__indicators-current').html(indicatorCurrentText);
+      (0, _jquery.default)(this).find($from).attr('aria-hidden', 'true');
+      (0, _jquery.default)(this).find($to).removeAttr('aria-hidden');
+      (0, _jquery.default)(this).find($fromIndicator).find('.carousel-tools__indicators-current').html('');
+      (0, _jquery.default)(this).find($toIndicator).find('.carousel-tools__indicators-current').html(indicatorCurrentText);
     });
     $this.find('a[data-modal="true"]').on('click', function () {
-      openCarouselPopup($this);
+      (0, _abcPopup.openCarouselPopup)($this);
     });
     $this.find('.carousel-tools__play-pause-button').on('click', function (e) {
-      let pauseText = $(this).data('pause');
-      let playText = $(this).data('play');
-      $(this).tooltip('dispose');
+      var pauseText = (0, _jquery.default)(this).data('pause');
+      var playText = (0, _jquery.default)(this).data('play');
+      (0, _jquery.default)(this).tooltip('dispose');
 
-      if ($(this).hasClass('paused')) {
-        $(this).toggleClass('paused');
-        $(elementId).carousel('cycle');
-        let text = pauseText;
-        $(this).attr('data-original-title', text);
-        $(this).find('span').text(text);
+      if ((0, _jquery.default)(this).hasClass('paused')) {
+        (0, _jquery.default)(this).toggleClass('paused');
+        (0, _jquery.default)(elementId).carousel('cycle');
+        var text = pauseText;
+        (0, _jquery.default)(this).attr('data-original-title', text);
+        (0, _jquery.default)(this).find('span').text(text);
       } else {
-        $(this).toggleClass('paused');
-        $(elementId).carousel('pause');
-        let text = playText;
-        $(this).attr('data-original-title', text);
-        $(this).find('span').text(text);
+        (0, _jquery.default)(this).toggleClass('paused');
+        (0, _jquery.default)(elementId).carousel('pause');
+        var _text = playText;
+        (0, _jquery.default)(this).attr('data-original-title', _text);
+        (0, _jquery.default)(this).find('span').text(_text);
       }
 
-      $(this).tooltip('enable');
+      (0, _jquery.default)(this).tooltip('enable');
     });
     $this.find('.carousel-tools__hide-button').on('click', function (e) {
-      let $toggleElements = $(elementId).find('.carousel-player, .carousel-descriptions, .carousel-tools__play-pause-button, .carousel-tools__indicators');
-      let hideText = $(this).data('hide');
-      let showText = $(this).data('show');
+      var $toggleElements = (0, _jquery.default)(elementId).find('.carousel-player, .carousel-descriptions, .carousel-tools__play-pause-button, .carousel-tools__indicators');
+      var hideText = (0, _jquery.default)(this).data('hide');
+      var showText = (0, _jquery.default)(this).data('show');
 
-      if ($(this).hasClass('hidden')) {
-        $(this).toggleClass('hidden');
-        $(elementId).carousel('cycle');
-        let text = hideText;
-        $(this).attr('data-original-title', text);
-        $(this).find('span').text(text);
-        $($toggleElements).show();
+      if ((0, _jquery.default)(this).hasClass('hidden')) {
+        (0, _jquery.default)(this).toggleClass('hidden');
+        (0, _jquery.default)(elementId).carousel('cycle');
+        var text = hideText;
+        (0, _jquery.default)(this).attr('data-original-title', text);
+        (0, _jquery.default)(this).find('span').text(text);
+        (0, _jquery.default)($toggleElements).show();
       } else {
-        $(this).toggleClass('hidden');
-        $(elementId).carousel('pause');
-        let text = showText;
-        $(this).attr('data-original-title', text);
-        $(this).find('span').text(text);
-        $($toggleElements).hide();
+        (0, _jquery.default)(this).toggleClass('hidden');
+        (0, _jquery.default)(elementId).carousel('pause');
+        var _text2 = showText;
+        (0, _jquery.default)(this).attr('data-original-title', _text2);
+        (0, _jquery.default)(this).find('span').text(_text2);
+        (0, _jquery.default)($toggleElements).hide();
       }
     });
   });
 }; // INIT
 
 
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-  initDescriptionPlacement();
+(0, _jquery.default)(function () {
+  (0, _jquery.default)('[data-toggle="tooltip"]').tooltip();
+  (0, _abcDescriptions.initDescriptionPlacement)();
   initAccessibleCarousels();
-  initCarouselPopupLayout();
+  (0, _abcPopup.initCarouselPopupLayout)();
 }); // ON RESIZE
 
-$(window).on('resize', () => {
-  calculateDescriptionHeight();
+(0, _jquery.default)(window).on('resize', function () {
+  (0, _abcDescriptions.calculateDescriptionHeight)();
 });
